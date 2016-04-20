a756fa692f3f99230bfc1186b6e5694ff2fbf361
avicennax <simon.haxby@gmail.com>
2016-04-19 20:54:20 -0700

    1 step closer to math pdfs

--- jedi.py
+++ jedi.py
@@ -3,18 +3,19 @@
 # Authors: Erik Peterson, Simon Haxby
 
 from __future__ import division
+import types
 import numpy as np
 from scipy.integrate import ode
-from numpy import eye, tanh, dot, outer, zeros_like
+from numpy import eye, tanh, dot, outer, zeros_like, zeros
 
 
-def force(target, model, lr, dt, tmax, tstop, x0, w):
+def force(target, model, lr, dt, tmax, tstop, x0, w, inputs=None, ode_solver=None, solver_params=None):
     """
     Abbott's FORCE algorithm.
 
     Parameters
     ----------
-        target: function
+        target: function / list
             Signal for network to learn.
         model: function
             ODE mass model.
@@ -30,7 +31,19 @@ def force(target, model, lr, dt, tmax, tstop, x0, w):
             Initial model state.
         w: ndarray
             Initial weight vector to be fit.
-
+        inputs: ndarray (Optional)
+            Model inputs at each time-step.
+        target_func: bool
+            Specifies whether the target is a function or ndarray.
+        ode_solver: scipy.integrate.ode (Optional)
+            Pre-initialized ODE solver.
+            NOTE: if solver_params not passed, then ode_solver will
+            be ignored and a new solver will be initialized.
+        solver_params: dict (Optional)
+            Contains state and time variables from a previous system.
+            Should contain:
+                'x': ndarray
+                't': ndarray
     Returns
     -------
         x: ndarray
@@ -46,36 +59,65 @@ def force(target, model, lr, dt, tmax, tstop, x0, w):
             (as induced by RLS).
     """
 
-    # Simulation data: state, output, time, weight updates
-    x, z, t, wu = [x0], [], [0], [0]
-
     # Running estimate of the inverse correlation matrix
     P = eye(len(x0))
 
     # Set up ode solver
-    solver = ode(model)
-    solver.set_initial_value(x0)
+    if ode_solver is None:
+        solver = ode(model)
+        solver.set_initial_value(x0)
+
+        # Simulation data: state, output, time, weight updates
+        x, z, t, wu = [x0], [], [0], [0]
+    else:
+        if solver_params is None:
+            x, z, t, wu = [x0], [], [0], [0]
+        else:
+            solver = ode_solver
+            t = solver_params['t']
+            x = solver_params['x'][-1]
+            wu, z = [], [0]
+
+    if inputs is None:
+        inputs = zeros(tmax).tolist()
+
+    if isinstance(target, types.FunctionType):
+        target_func = True
+    else:
+        target_func = False
+
+
+    prev_tmax = t[-1]
+    index = 0
 
     # Integrate ODE, update weights, repeat
-    while t[-1] < tmax:
+    while t[-1] < tmax + prev_tmax:
         tanh_x = tanh(x[-1])  # cache
         z.append(dot(w, tanh_x))
-        error = target(t[-1]) - z[-1]
+
+        if target_func:
+            error = target(t[-1]) - z[-1]
+        else:
+            error = target[index] - z[-1]
+
         q = dot(P, tanh_x)
         c = lr / (1 + dot(q, tanh_x))
         P = P - c * outer(q, q)
         w = w + c * error * q
 
         # Stop leaning here
-        if t[-1] > tstop:
+        if t[-1] > tstop + prev_tmax:
             lr = 0
 
         wu.append(np.sum(np.abs(c * error * q)))
 
-        solver.set_f_params(tanh_x, w)
+        solver.set_f_params(tanh_x, w, inputs[index], z[-1])
         solver.integrate(solver.t + dt)
         x.append(solver.y)
         t.append(solver.t)
+
+        # Allows for next input/target to be processed.
+        index += 1
 
     # last update for readout neuron
     z.append(dot(w, tanh_x))
@@ -93,7 +135,7 @@ def decode(x, rho):
 
     return xd
 
-def dforce(rho, target, model, lr, dt, tmax, tstop, x0, w):
+def dforce(rho, target, model, lr, dt, tmax, tstop, x0, w, inputs=None, ode_solver=None, solver_params=None):
     """
     Peterson's DFORCE algorithm.
     A.K.A Abbott's FORCE with binary thresholding.
@@ -102,7 +144,7 @@ def dforce(rho, target, model, lr, dt, tmax, tstop, x0, w):
     ----------
         rho: float
             Binary threshold for decode.
-        target: function
+        target: function / ndarray
             Signal for network to learn.
         model: function
             ODE mass model.
@@ -118,6 +160,19 @@ def dforce(rho, target, model, lr, dt, tmax, tstop, x0, w):
             Initial model state.
         w: ndarray
             Initial weight vector to be fit.
+        inputs: ndarray (Optional)
+            Model inputs at each time-step.
+        target_func: bool
+            Specifies whether the target is a function or ndarray.
+        ode_solver: scipy.integrate.ode (Optional)
+            Pre-initialized ODE solver.
+            NOTE: if solver_params not passed, then ode_solver will
+            be ignored and a new solver will be initialized.
+        solver_params: dict (Optional)
+            Contains state and time variables from a previous system.
+            Should contain:
+                'x': ndarray
+                't': ndarray
 
     Returns
     -------
@@ -134,43 +189,70 @@ def dforce(rho, target, model, lr, dt, tmax, tstop, x0, w):
             (as induced by RLS).
     """
 
-    # Simulation data: state, output, time, weight updates
-    x, z, t, wu = [x0], [], [0], [0]
-
     # Running estimate of the inverse correlation matrix
     P = eye(len(x0))
 
     # Set up ode solver
-    solver = ode(model)
-    solver.set_initial_value(x0)
+    if ode_solver is None:
+        solver = ode(model)
+        solver.set_initial_value(x0)
+
+        # Simulation data: state, output, time, weight updates
+        x, z, t, wu = [x0], [], [0], [0]
+    else:
+        solver = ode_solver
+        t = solver_params['t']
+        x = solver_params['x'][-1]
+        wu, z = [], [0]
+
+    if inputs is None:
+        inputs = zeros(tmax).tolist()
+
+    if isinstance(target, types.FunctionType):
+        target_func = True
+    else:
+        target_func = False
 
 
-    # Integrate ode, update weights, repeat
-    while t[-1] < tmax:
+    prev_tmax = t[-1]
+    index = 0
+
+    # Integrate ODE, update weights, repeat
+    while t[-1] < tmax + prev_tmax:
         tanh_x = tanh(x[-1])
         tanh_xd = decode(tanh_x, rho)  # BINARY CODE INTRODUCED HERE!
         z.append(dot(w, tanh_xd))
-        error = target(t[-1]) - z[-1]
+
+        if target_func:
+            error = target(t[-1]) - z[-1]
+        else:
+            error = target[index] - z[-1]
+
         q = dot(P, tanh_xd)
         c = lr / (1 + dot(q, tanh_xd))
         P = P - c * outer(q, q)
         w = w + c * error * q
 
-        # Stop training time
-        if t[-1] > tstop:
+        # Stop leaning here
+        if t[-1] > tstop + prev_tmax:
             lr = 0
 
         wu.append(np.sum(np.abs(c * error * q)))
 
-        solver.set_f_params(tanh_x, w)
+        solver.set_f_params(tanh_x, w, inputs[index], z[-1])
         solver.integrate(solver.t + dt)
         x.append(solver.y)
         t.append(solver.t)
 
+        # Allows for next input to be processed.
+        index += 1
+
     # last update for readout neuron
-    z.append(dot(w, tanh_x))
+    z.append(dot(w, tanh_xd))
 
     x = np.array(x)
     t = np.array(t)
 
     return x, t, z, w, wu
+
+
